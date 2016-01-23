d94d1ad29daf4ab2db4f481cbe0e3008cae522e5
Andrea Sonny <andreasonny83@gmail.com>
2016-01-23 17:22:08 +0000

    another test

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
       'src/bower_components/angular-route/angular-route.js',
       'src/bower_components/angular-mocks/angular-mocks.js',
       'src/bower_components/angular-mocks/angular-mocks.js',
-      'app/**/*.js',
+      'src/app/**/*.js',
       'test/**/*.js'
     ],
 
